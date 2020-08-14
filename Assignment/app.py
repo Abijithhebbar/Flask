97cc209278f31b5159cf2d89acdfb98f1793f428
Abijith Hebbar <41896691+Abijithhebbar@users.noreply.github.com>
2020-08-15 00:02:31 +0530

    Add files via upload

---
+++ Assignment/app.py
@@ -0,0 +1,40 @@
+import os
+import json
+from flask import Flask, render_template, url_for, request, redirect, jsonify
+from PIL import Image
+
+
+Upload = 'static/upload'
+app = Flask(__name__)
+app.config['uploadFolder'] = Upload
+
+
+@app.route("/")
+def index():
+	return render_template("Index.html")
+
+@app.route("/upload", methods = ['POST', 'GET'])
+def upload():
+	file = request.files['imgfile']
+	filename = file.filename
+	file.save(os.path.join(app.config['uploadFolder'], file.filename))
+	img = Image.open(file)
+	li = []
+	li = img.size
+	imgobj = {
+	"name" : filename,
+	"width" : li[0],
+	"height" : li[1]
+	}
+	json_data = json.dumps(imgobj)
+	with open('jsonfile.json', 'w') as json_file:
+		json.dump(imgobj, json_file)
+	print(json_data)
+	print(type(json_data))
+	return render_template('index.html', filename = filename)
+
+
+
+
+if __name__ == "__main__":
+	app.run(debug = True)
