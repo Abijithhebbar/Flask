082379d9ac8b7535e7645d38772435ecfae69f81
Abijith Kattangeri <abijith.kattangeri@gramener.org>
2020-09-11 00:56:52 +0530

    commit

---
+++ Assignment 2/app.py
@@ -0,0 +1,46 @@
+import os
+import json
+import requests
+from flask import Flask, render_template, url_for, request
+
+
+Upload = 'D:\\Flask\\Assignment 2'
+app = Flask(__name__)
+app.config['uploadFolder'] = Upload
+
+
+@app.route("/")
+def index():
+	return render_template("Index.html")
+
+@app.route("/upload", methods = ['POST', 'GET'])
+def upload():
+	"""
+	Takes the image from the user
+	and sends that to the rest api
+	and gets the image name height and width
+	"""
+	file = request.files['imgfile']
+
+	filename = file.filename
+	file.save(os.path.join(app.config['uploadFolder'], file.filename))
+	files = {'image': open(filename, 'rb')}
+	url = "http://127.0.0.1:4555/image"
+	response = requests.request("POST", url, files=files)
+	print(response)
+	try:
+		jsonfile = response.json()
+	except:
+		json_data = {
+		"name" : "Not supported"
+		}
+		jsonfile = json.dumps(json_data)
+
+		
+	return render_template('index.html', json_data = jsonfile) 
+
+
+
+
+if __name__ == "__main__":
+	app.run(debug = True)
