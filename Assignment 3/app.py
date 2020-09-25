5f406e8ff723efe4296e45f2199fe512b577649d
Abijith Kattangeri <abijith.kattangeri@gramener.org>
2020-09-25 12:26:51 +0530

    updated with clean code

--- Assignment 3/app.py
+++ Assignment 3/app.py
@@ -9,16 +9,23 @@ Upload = 'static/upload'
 app = Flask(__name__)
 app.config['uploadFolder'] = Upload
 
-
+"""
+Renders the Home page of the application.
+"""
 @app.route("/", methods = ['POST', 'GET'])
 def index():
 	return render_template("Index.html")
-
+"""
+It gets the files from the index.html page.
+Sends the image to REST API and gets the image height width and image name as json from the REST API.
+The Image name height and width are sent to the Index1.html page
+where we display the images in a carousel with svg boundaries.
+"""
 @app.route("/upload", methods = ['POST', 'GET'])
 def upload():
 	jsonfile = []
 	filenames = []
-	uploaded_files = request.files.getlist("file[]")
+	uploaded_files = request.files.getlist("Imagefiles[]")
 	slider_value = request.form["value_of_slider"]
 	for file in uploaded_files:
 		filename = file.filename
@@ -27,8 +34,8 @@ def upload():
 		os.chdir(downloadpath)
 		files = {'image': open(filename, 'rb')}
 		url = "http://127.0.0.1:4555/image"
-		response = requests.request("POST", url, files=files)
-		jsonfile.append(response.json())
+		response = requests.request("POST", url, files = files) # gets the response from the REST API
+		jsonfile.append(response.json()) # gets the json output of the response from REST API
 		filenames.append(filename)
 		normalpath = "D:\\Flask\\Assignment 3"
 		os.chdir(normalpath)
