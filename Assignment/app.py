749dac3cca81bbde259077b5e10b055ef9ee93f7
Abijith Kattangeri <abijith.kattangeri@gramener.org>
2020-09-28 18:49:55 +0530

    updated with clean code

--- Assignment/app.py
+++ Assignment/app.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template, url_for, request, redirect, jsonify
+from flask import Flask, render_template, jsonify, request
 from PIL import Image
 
 
@@ -8,34 +8,54 @@ Upload = 'static/upload'
 app = Flask(__name__)
 app.config['uploadFolder'] = Upload
 
-
+"""
+Flask app home page
+"""
 @app.route("/")
-def index():
+
+def start():
 	return render_template("Index.html")
 
+"""
+Takes the image from the user.
+sends the image to writeToJson method and displays the uploaded image.
+"""
 @app.route("/upload", methods = ['POST', 'GET'])
-def upload():
-	file = request.files['imgfile']
-	filename = file.filename
-	file.save(os.path.join(app.config['uploadFolder'], file.filename))
-	img = Image.open(file)
-	li = []
-	li = img.size
-	imgobj = {
-	"name" : filename,
-	"width" : li[0],
-	"height" : li[1]
-	}
-	json_data = json.dumps(imgobj)
-	with open('jsonfile.json', 'w') as json_file:
-		json.dump(imgobj, json_file)
-	print(json_data)
-	print(type(json_data))
-	return render_template('index.html', filename = filename) # comment this line to only get the json data.
-	# return render_template('index.html', json_data = json_data) # uncomment this line to get only json data.
 
+def getImage():
+	imagefile = request.files['image']
+	filename = imagefile.filename
+	imagefile.save(os.path.join(app.config['uploadFolder'], filename))
+	writeToJson(filename)
+	return render_template('index.html', filename = filename)
+
+"""
+Is called by getImage method.
+get the json data of the image from getData method and writes it to json file.
+"""
+def writeToJson(imagefile):
+	imageobj = getData(imagefile)
+	with open('jsonfile.json', 'w') as json_file:
+		json.dump(imageobj, json_file)
+
+"""
+Is called by writeToJson method.
+gets the image name from writeToJson method.
+opens the image using the Pillow library and gets the height and width of the image and returns that to writeToJson method.
+"""
+def getData(imagefile):
+	os.chdir("D:\\Flask\\Assignment\\static\\upload")
+	img = Image.open(imagefile)
+	width, height = img.size
+	imgobj = {
+	"name" : imagefile,
+	"width" : width,
+	"height" : height
+	}
+	os.chdir("D:\\Flask\\Assignment")
+	return imgobj
 
 
 
 if __name__ == "__main__":
-	app.run(debug = True)
+	app.run(debug = True, port = 4455)
